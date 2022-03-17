be95666978153a56607d0e1ee624beac542cb24a
Ryota Ishibashi <ryota.ishibashi@ryo-channel.dev>
2022-03-17 08:22:17 +0000

    chore: コメント追加

--- Assets/Scripts/UICraft.cs
+++ Assets/Scripts/UICraft.cs
@@ -14,18 +14,19 @@ public class UICraft : MonoBehaviour
     private ItemDatabase itemDB;
     private List<Item> craftables;
 
+    // ①スタート時、クラフトできるアイテムのスロットを生成する
     private void Awake()
-    {
+    {    
         itemDB = GameObject.Find("ItemDatabase").GetComponent<ItemDatabase>();
-        craftables = itemDB.items.FindAll(item => item.craftable);
+        craftables = itemDB.items.FindAll(item => item.craftable); // itemDB内でクラフト可能なアイテムのみ選択する
         foreach (var item in craftables)
         {
             GameObject instance = Instantiate(slotPrefab);
             instance.transform.SetParent(slotPanel);
             instance.GetComponentInChildren<UIItem>().UpdateItem(item);
-            instance.GetComponentInChildren<UIItem>().OnClick.AddListener(OnClickItem);
+            instance.GetComponentInChildren<UIItem>().OnClick.AddListener(OnClickItem);  // アイテムがクリックされた時動かす関数(内容はOnClickItem参照)
         }
-        craftButton.interactable = false;
+        craftButton.interactable = false;   // クラフトするボタンは無効にしておく
     }
 
     // クラフトに必要なアイテムの表示を全て削除する
@@ -38,23 +39,37 @@ public class UICraft : MonoBehaviour
         }
     }
 
+    // ②クラフト可能なアイテムリストのアイテムをクリックしたら、必要なアイテムリストにアイテムを描画していく
     public void OnClickItem(UIItem uiItem)
     {
+        // 一応、クラフトできるアイテムかどうかをチェック（たぶんいらない）
         if (uiItem.item.craftable)
         {
+            // まずは、クラフトに必要なものリストのPanelをきれいにする
             ClearRequiredItems();
+
+            // クラフト可能かどうかのフラグ
             bool isCraftable = true;
             foreach (var item in uiItem.item.requiredItems)
             {
+                // アイテムスロットの生成・描画
                 GameObject instance = Instantiate(slotPrefab);
                 instance.transform.SetParent(requiredItemsSlotPanel);
                 instance.GetComponentInChildren<UIItem>().UpdateItem(item);
+                // インベントリに必要なアイテムを持っていなかったらフラグをfalseにする
                 isCraftable = isCraftable && inventory.CheckForItem(item.id) != null;
             }
+
+            // isCraftableフラグがtrueの時はボタンが押せるようにする。falseの時はボタンが押せなくなる。
             craftButton.interactable = isCraftable;
+
+            // クラフトボタンが押された時の処理。
             craftButton.onClick.AddListener(() => {
+                // インベントリからアイテムを削除
                 inventory.RemoveItems(uiItem.item.requiredItems);
+                // インベントリにクラフトしたアイテムを追加
                 inventory.GiveItem(uiItem.item.id);
+                // クラフトボタンを無効にする
                 craftButton.interactable = false;
             });
         }
